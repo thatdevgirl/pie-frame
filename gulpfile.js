0278839453b739a04333378910a715d1daf77c6f
Joni Halabi <her@thatdevgirl.com>
2016-12-07 14:36:15 -0500

    Creating package file and updating dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,26 @@ var gulp   = require('gulp'),
     watch  = require('gulp-watch');
 
 // Array of JS files, in order by dependency.
+var vendorFiles = [
+  'node_modules/jquery/dist/jquery.min.js',
+  'node_modules/d3/build/d3.min.js',
+  'node_modules/randomcolor/randomcolor.js'
+];
+
 var jsFiles = [
   'js/src/pieFrame.js',
   'js/src/pfBarChart.js',
   'js/src/pfPieChart.js'
  ];
 
-// Task to concatinate and minify JS files.
+// Task to concatinate required vendor JS files.
+gulp.task('vendor', function() {
+  return gulp.src(vendorFiles)
+    .pipe(concat('pieFrame-vendor.min.js'))
+    .pipe(gulp.dest('js'));
+});
+
+// Task to concatinate and minify custom JS files.
 gulp.task('js', function() {
   return gulp.src(jsFiles)
     .pipe(babel({presets: ['es2015']}))
@@ -27,4 +40,4 @@ gulp.task('watch', function() {
 });
 
 // Default task
-gulp.task('default', ['watch']);
+gulp.task('default', ['vendor', 'js', 'watch']);
